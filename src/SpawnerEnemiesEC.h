4aec981ed5c6122f943f5cf277f5a5876e1b1a65
GonzaPM7 <gonzap03@ucm.es>
2020-04-03 18:31:39 +0200

    create limit

--- src/SpawnerEnemiesEC.h
+++ src/SpawnerEnemiesEC.h
@@ -9,9 +9,11 @@ class TransformComponent;
 class SpawnerEnemiesEC : public SpawnerEC {
   private:
     TransformComponent* transform = nullptr;
+    int enemies;
 
   public:
     virtual void checkEvent() override;
 
     void setTransform(TransformComponent* trans);
+    void setEnemies(int _enemies);
 };
