d6c20313bb7b3aa8c82a4a31d2ac4087fc57e2d8
Dankgard <dangar18@ucm.es>
2020-04-09 18:49:53 +0200

    Missing doDamage call adapted

--- src/BulletHurtPlayerEC.cpp
+++ src/BulletHurtPlayerEC.cpp
@@ -25,7 +25,10 @@ void BulletHurtPlayerEC::checkEvent() {
         int damage = bullet->getDamage();
         LifeC* playerHealth = dynamic_cast<LifeC*>(
             scene->getEntitybyId("Player")->getComponent("LifeC"));
-        // playerHealth->doDamage(damage);
+        /* // if player dies sleep method is called
+        if (playerHealth->doDamage(damage))
+            ;
+        // TO DO: call sleep() */
 
         // destroy bullet
         bullet->dealCollision();
