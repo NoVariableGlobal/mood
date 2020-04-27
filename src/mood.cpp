7d8a6bdd68431ddb2c9f710a946f170dac7ed97a
pablojor <pablojor@ucm.es>
2020-04-27 13:22:30 +0200

    update

--- src/mood.cpp
+++ src/mood.cpp
@@ -14,7 +14,8 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, LPSTR lmCmdLine,
     try {
         srand(time(nullptr));
         Game game;
-        if (game.init("MOOD"))
+        if (game.init("MOOD", "TaharezLook.scheme", "TaharezLook/MouseArrow",
+                      "DejaVuSans-14"))
             game.run();
     } catch (std::exception& e) {
         std::cout << "\n"
