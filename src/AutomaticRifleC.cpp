48eefe3fd9bc4237dd4d449066a9e33c40edbf3c
onaranjoUCM <37249723+onaranjoUCM@users.noreply.github.com>
2020-05-10 11:47:41 +0100

    Added gun models to player (#36)
    * Change model with gun change
    
    * Gun models working
    
    * Enemy hitboxes increased
    
    * Format

--- src/AutomaticRifleC.cpp
+++ src/AutomaticRifleC.cpp
@@ -16,6 +16,8 @@ void AutomaticRifleC::onShoot(TransformComponent* transform,
     Ogre::Quaternion quat = getOrientation();
 
     transform->setPosition(myTransform->getPosition() +
+                           (quat * Ogre::Vector3::UNIT_X) * -2 +
+                           (quat * Ogre::Vector3::UNIT_Y) * 25 +
                            (quat * Ogre::Vector3::UNIT_Z) * 10);
     transform->setOrientation(myTransform->getOrientation());
 
