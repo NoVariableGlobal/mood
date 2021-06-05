22ca1863c6b74760eb2b60b88bfa909998853678
Antonio Román <kyradiscord@gmail.com>
2021-06-05 09:39:03 +0200

    chore: updated OTY, fixed CI and setup

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,6 +1,5 @@
 # Set up the submodule so dependencies are properly included in the project:
-git submodule init
-git submodule update
+git submodule update --init --recursive
 
 # Define the hooks patch as $root/hooks over $root/.git/hooks so we can run our own hooks:
 git config core.hooksPath hooks
